97f6293aaf73f6d56d4e50839000b68c77fd7be6
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-10-09 11:57:38 +0300

    .

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> clips;
+
+    private AudioSource musicSource;
+    private void Awake()
+    {
+        musicSource = GetComponent<AudioSource>();
+        PlayRandomMusic();
+    }
+    private void PlayRandomMusic()
+    {
+        var clip = clips[Random.Range(0, clips.Count)];
+        musicSource.PlayOneShot(clip);
+    }
+}
